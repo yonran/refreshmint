91c915a59d8771c46efccaa44175299a04f303f7
Yonathan Randolph <yonathan@gmail.com>
2026-02-10 22:57:23 -0800

    prettier --write .

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
 if (!root) {
-  throw new Error('Root element not found')
+    throw new Error('Root element not found');
 }
 
 createRoot(root).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3003, // instead of 5173; must match tauri.conf.json
-  }
-})
+    plugins: [react()],
+    server: {
+        port: 3003, // instead of 5173; must match tauri.conf.json
+    },
+});
