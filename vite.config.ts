369d78d1aaea7a79eaaf0a5c680e3cc7db654101
Yonathan Randolph <yonathan@gmail.com>
2026-02-10 20:49:24 -0800

    Use consistent port

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3003, // instead of 5173; must match tauri.conf.json
+  }
 })
